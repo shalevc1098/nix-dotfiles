a4d639745e3c9611f8f8fa5b4deca256b7b9e9e3
shalevc1098 <shalevc8901@gmail.com>
2025-09-09 19:39:04 +0300

    updated hyprlock configuration

--- modules/hypr/hyprlock/home.nix
+++ modules/hypr/hyprlock/home.nix
@@ -9,11 +9,6 @@ with config.wayland.windowManager.hyprland.settings;
   programs.hyprlock = {
     enable = true;
     # package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
-    settings = {
-      background = {
-        color = "rgba(25, 20, 20, 1.0)";
-      };
-    };
   };
 
   # xdg.configFile."hypr/hyprlock.conf".source = builtins.toString ./hyprlock.conf;
