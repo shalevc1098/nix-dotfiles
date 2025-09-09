e5f18ff1e522b48862c6d1e5c650611f410f0b1d
shalevc1098 <shalevc8901@gmail.com>
2025-09-09 19:39:04 +0300

    updated cliphist configuration

--- modules/cliphist/home.nix
+++ modules/cliphist/home.nix
@@ -2,10 +2,17 @@
 {
   home.packages = with pkgs; [
     cliphist
+    wl-clipboard
   ];
 
-  wayland.windowManager.hyprland.settings.exec-once = [
-    "wl-paste --type text --watch cliphist store"
-    "wl-paste --type image --watch cliphist store"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "wl-paste --type text --watch cliphist store"
+      "wl-paste --type image --watch cliphist store"
+    ];
+    
+    bind = [
+      "Super, Super_L, exec, pgrep quickshell && noctalia-shell ipc call launcher toggle || (pgrep wofi && pkill wofi || wofi)"
+    ];
+  };
 }
