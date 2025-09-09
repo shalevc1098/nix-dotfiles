43db32288fb548e33c5a743af6239f04af338a99
shalevc1098 <shalevc8901@gmail.com>
2025-09-09 19:39:04 +0300

    updated hypridle configuration

--- modules/hypr/hypridle/home.nix
+++ modules/hypr/hypridle/home.nix
@@ -7,17 +7,17 @@
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
-      # listener = [
-      #   {
-      #     timeout = 180;
-      #     on-timeout = "loginctl lock-session";
-      #   }
-      #   {
-      #     timeout = 240;
-      #     on-timeout = "hyprctl dispatch dpms off";
-      #     on-resume = "hyprctl dispatch dpms on";
-      #   }
-      # ];
+      listener = [
+        {
+          timeout = 180;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 240;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
     };
   };
 }
