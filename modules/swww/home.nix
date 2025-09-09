f1fff27a4eccb3308b0d6e5fdfb2cdf624162df9
shalevc1098 <shalevc8901@gmail.com>
2025-09-09 19:39:04 +0300

    updated swww configuration

--- modules/swww/home.nix
+++ modules/swww/home.nix
@@ -6,6 +6,6 @@
 
   wayland.windowManager.hyprland.settings.exec-once = [
     "swww-daemon --format xrgb"
-    "exec-once = sleep 0.5; swww img ~/wallpaper.jpg --transition-step 100 --transition-fps 120 --transition-type grow --transition-angle 30 --transition-duration 1" # TODO: remove
+    # "exec-once = sleep 0.5; swww img ~/wallpaper.png --transition-step 100 --transition-fps 120 --transition-type grow --transition-angle 30 --transition-duration 1" # TODO: remove
   ];
 }
