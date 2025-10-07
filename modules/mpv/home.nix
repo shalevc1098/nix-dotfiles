29a10f5f9a3e223ea6e8e0c15056334c08294006
shalevc1098 <shalevc8901@gmail.com>
2025-10-07 16:15:47 +0300

    added mpv video player with hardware decoding enabled

---
+++ modules/mpv/home.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs.mpv = {
+    enable = true;
+    config = {
+      # Audio settings
+      volume-max = 150;
+      volume = 100;
+
+      # Video settings
+      hwdec = "auto";
+      vo = "gpu";
+
+      # Interface
+      osd-level = 1;
+      osd-duration = 2500;
+    };
+  };
+}
