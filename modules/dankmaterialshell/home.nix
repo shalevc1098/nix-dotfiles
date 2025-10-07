f129ca2252dedba0e60b11540b4f474a7bd96cf8
shalevc1098 <shalevc8901@gmail.com>
2025-10-07 16:15:47 +0300

    disabled dynamic theming and added spotlight launcher with reload keybind

--- modules/dankmaterialshell/home.nix
+++ modules/dankmaterialshell/home.nix
@@ -1,12 +1,23 @@
 { inputs, ... }:
 {
   imports = [
-    inputs.dankmaterialshell.homeModules.dankMaterialShell
-    inputs.niri.homeModules.niri
+    inputs.dankmaterialshell.homeModules.dankMaterialShell.default
+    # inputs.niri.homeModules.niri
   ];
 
   programs.dankMaterialShell = {
     enable = true;
-    enableDynamicTheming = true;
+    enableDynamicTheming = false;
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "dms run &"
+    ];
+    
+    bind = [
+      "Super, Super_L, exec, pgrep quickshell && dms ipc call spotlight toggle || (pgrep wofi && pkill wofi || wofi)"
+      "Ctrl+Super, R, exec, pkill quickshell; sleep 0.2 && dms run &"
+    ];
   };
 }
