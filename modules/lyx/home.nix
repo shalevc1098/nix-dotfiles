b6ea673766e178a50f3333e819d2575891be7220
shalevc1098 <shalevc8901@gmail.com>
2025-10-31 21:58:26 +0200

    compiled lyx with qt6 to fix an issue where dolphin's folders were completely black

--- modules/lyx/home.nix
+++ modules/lyx/home.nix
@@ -4,7 +4,22 @@
 }:
 {
   home.packages = with pkgs; [
-    lyx
+    ((kdePackages.callPackage "${pkgs.path}/pkgs/applications/misc/lyx" {
+      inherit (kdePackages) qtbase qtsvg;
+      mkDerivation = stdenv.mkDerivation;
+    }).overrideAttrs (oldAttrs: {
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ kdePackages.wrapQtAppsHook ];
+      configureFlags = [ "--enable-qt6" ] ++ (builtins.filter (flag: flag != "--enable-qt5") oldAttrs.configureFlags);
+
+      # Patch Qt6 incompatibility in GuiSymbols.cpp
+      postPatch = (oldAttrs.postPatch or "") + ''
+        substituteInPlace src/frontends/qt/GuiSymbols.cpp \
+          --replace-fail '.arg(QString("%1").arg(c, 0, 16).toUpper())' '.arg(QString::number(static_cast<uint>(c), 16).toUpper())'
+      '';
+
+      # Skip tests - they have linking issues with Qt6
+      doCheck = false;
+    }))
 
     (texlive.combine {
       inherit (texlive)
