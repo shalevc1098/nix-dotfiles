ae6d99a43db27ad2e904f5777c6e4a2858fed82e
shalevc1098 <shalevc8901@gmail.com>
2025-11-07 18:44:23 +0200

    commented iconTheme

--- modules/theme/gtk.nix
+++ modules/theme/gtk.nix
@@ -15,10 +15,10 @@
       package = pkgs.adw-gtk3;
     };
 
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
+    # iconTheme = {
+    #   name = "Papirus-Dark";
+    #   package = pkgs.papirus-icon-theme;
+    # };
 
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
