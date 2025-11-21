ce04ec6bd26c1dd02d9bf18e0f9ff10088feda32
shalevc1098 <shalevc8901@gmail.com>
2025-11-21 20:40:17 +0200

    added kio-admin

--- modules/kde/dolphin/nixos.nix
+++ modules/kde/dolphin/nixos.nix
@@ -6,13 +6,15 @@
   environment.systemPackages = with pkgs; [
     kdePackages.dolphin
     kdePackages.ffmpegthumbs
+    kdePackages.kio-admin
   ];
 
   # Fix Dolphin's "Open with" menu not opening at all
   environment.pathsToLink = [ "/share" ];
 
   # Fix Dolphin's "Open with" menu by providing the applications menu file
-  environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  environment.etc."xdg/menus/applications.menu".source =
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
   # For cache to rebuild on each rebuild
   system.userActivationScripts.rebuildSycoca = ''
