6212095c0b89afe193b5e880fb26fdfc557960ba
shalevc1098 <shalevc8901@gmail.com>
2025-11-21 15:12:44 +0200

    added samba for file sharing between host and guest vm

--- modules/virtualization/samba/nixos.nix
+++ modules/virtualization/samba/nixos.nix
@@ -1,5 +1,29 @@
 {
+  username,
+  ...
+}:
+{
   services.samba = {
     enable = true;
+    settings = {
+      global = {
+        "allow insecure wide links" = "yes";
+        "unix extensions" = "no";
+        "follow symlinks" = "yes";
+        "wide links" = "yes";
+        "map to guest" = "bad user";
+      };
+
+      home = {
+        path = "/home/${username}";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "guest only" = "yes";
+        "force user" = username;
+        "follow symlinks" = "yes";
+        "wide links" = "yes";
+      };
+    };
   };
 }
