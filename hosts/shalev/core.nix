61ccce0e26fa2b0845ab665bf0db4af9c0014483
shalevc1098 <shalevc8901@gmail.com>
2025-11-22 19:37:23 +0200

    switched to grub bootloader

--- hosts/shalev/core.nix
+++ hosts/shalev/core.nix
@@ -14,9 +14,22 @@
     ./hardware-configuration.nix
   ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Use the grub boot loader.
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+    };
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+
+      extraConfig = ''
+        terminal_output console
+      '';
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
