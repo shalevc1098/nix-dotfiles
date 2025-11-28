aeb6d2e893970dfcafc29185176a3ac8574e504d
shalevc1098 <shalevc8901@gmail.com>
2025-11-28 21:37:09 +0200

    increased download buffer size

--- hosts/shalev/core.nix
+++ hosts/shalev/core.nix
@@ -159,6 +159,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    download-buffer-size = 536870912;
   };
 
   # Allow unfree packages
