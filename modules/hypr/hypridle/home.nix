83f08b3279cf6fbcbf83996bd656c55efa60b154
shalevc1098 <shalevc8901@gmail.com>
2025-11-28 21:37:30 +0200

    removed wayland-pipewire-idle-inhibit

--- modules/hypr/hypridle/home.nix
+++ modules/hypr/hypridle/home.nix
@@ -8,9 +8,6 @@ let
   lock_cmd = "pidof hyprlock || uwsm app -- hyprlock";
 in
 {
-  imports = [
-    inputs.wayland-pipewire-idle-inhibit.homeModules.default
-  ];
   services.hypridle = {
     enable = true;
     settings = {
@@ -32,17 +29,4 @@ in
       ];
     };
   };
-
-  # services.wayland-pipewire-idle-inhibit = {
-  #   enable = true;
-  #   systemdTarget = "hyprland-session.target";
-  #   settings = {
-  #     verbosity = "INFO";
-  #     media_minimum_duration = 10;
-  #     idle_inhibitor = "wayland";
-  #     sink_whitelist = [
-  #       { name = "Arctis Nova Pro Wireless Analog Stereo"; }
-  #     ];
-  #   };
-  # };
 }
