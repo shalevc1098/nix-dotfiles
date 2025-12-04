75a45ede763bbd174e7496b631c90d43ae26cda2
shalevc1098 <shalevc8901@gmail.com>
2025-12-05 00:18:11 +0200

    made nvidia to use proprietary drivers because of a build error

--- hosts/shalev/core.nix
+++ hosts/shalev/core.nix
@@ -66,7 +66,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = true;
+    open = false;
     nvidiaSettings = true;
   };
 
