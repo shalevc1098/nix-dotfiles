afd1980d0597384a3f2255baa77a7a88b9b0dc81
shalevc1098 <shalevc8901@gmail.com>
2025-12-05 19:36:05 +0200

    added loopback

--- modules/system/audio/nixos.nix
+++ modules/system/audio/nixos.nix
@@ -4,5 +4,28 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire."99-loopback" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-loopback";
+          args = {
+            "audio.position" = [
+              "FL"
+              "FR"
+            ];
+            "capture.props" = {
+              "media.class" = "Audio/Sink";
+              "node.name" = "loopback_sink";
+              "node.description" = "Loopback Sink";
+            };
+            "playback.props" = {
+              "media.class" = "Audio/Source";
+              "node.name" = "loopback_source";
+              "node.description" = "Loopback Source";
+            };
+          };
+        }
+      ];
+    };
   };
 }
