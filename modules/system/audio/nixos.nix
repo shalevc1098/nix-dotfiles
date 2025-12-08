7f8834d5a1c72778d164843f5c36cdf5f612c9bc
shalevc1098 <shalevc8901@gmail.com>
2025-12-08 20:23:23 +0200

    set some defaults to the audio

--- modules/system/audio/nixos.nix
+++ modules/system/audio/nixos.nix
@@ -4,6 +4,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire."99-rate" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [ 48000 ];
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 1024;
+      };
+    };
     extraConfig.pipewire."99-loopback" = {
       "context.modules" = [
         {
