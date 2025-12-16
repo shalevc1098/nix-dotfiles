a0bf73ff6d5bd5630e3f3ea48fb4873f74f088b5
shalevc1098 <shalevc8901@gmail.com>
2025-12-16 18:42:53 +0200

    force killed hyprpicker after screenshot because sometimes it's not being killed

--- modules/screenshot/home.nix
+++ modules/screenshot/home.nix
@@ -12,8 +12,8 @@
 
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "Super+Shift, S, exec, grimblast --freeze copy area" # Region Screenshot (freeze screen while selecting)
-      "Super+Shift+Ctrl, S, exec, grimblast --freeze save area - | swappy -f -" # Region Screenshot with editor
+      "Super+Shift, S, exec, grimblast --freeze copy area; pkill -9 hyprpicker" # Region Screenshot (freeze screen while selecting)
+      "Super+Shift+Ctrl, S, exec, grimblast --freeze save area - | swappy -f -; pkill -9 hyprpicker" # Region Screenshot with editor
     ];
     bindl = [
       ", Print, exec, grimblast copy output" # Fullscreen Screenshot
