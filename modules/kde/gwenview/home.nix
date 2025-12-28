9f2de264de03b31d45c07da4ee2b97ef902ad245
shalevc1098 <shalevc8901@gmail.com>
2025-12-28 19:55:31 +0200

    centered gwenview window

--- modules/kde/gwenview/home.nix
+++ modules/kde/gwenview/home.nix
@@ -8,6 +8,7 @@
     windowrule = [
       "match:class org.kde.gwenview, float on"
       "match:class org.kde.gwenview, size (monitor_w*0.6) (monitor_h*0.65)"
+      "match:class org.kde.gwenview, center on"
     ];
   };
 
