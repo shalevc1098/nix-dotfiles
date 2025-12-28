c4af86f5a932c8f73db5046c7e30fa0504b74144
shalevc1098 <shalevc8901@gmail.com>
2025-12-28 19:47:17 +0200

    added window opacity for blur effect

--- modules/hypr/hyprland/config/configs/rules.nix
+++ modules/hypr/hyprland/config/configs/rules.nix
@@ -1,5 +1,8 @@
 {
   windowrule = [
+    # Blur all windows
+    "match:class .*, opacity 0.86 0.79"
+
     # Disable blur for XWayland windows (or context menus with shadow would look weird)
     "match:xwayland 1, no_blur on"
 
