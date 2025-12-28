e929a937415972a8e36d7a67ae8efadf3fcd0d25
shalevc1098 <shalevc8901@gmail.com>
2025-12-28 19:06:16 +0200

    added niri session option to greetd

--- modules/greetd/nixos.nix
+++ modules/greetd/nixos.nix
@@ -8,6 +8,7 @@
     settings = rec {
       initial_session = {
         command = "uwsm start hyprland-uwsm.desktop";
+        # command = "niri-session";
         user = "shalev";
       };
       default_session = initial_session;
