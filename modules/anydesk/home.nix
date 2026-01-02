6d529768560fa21b968db4959ea655ee4a8ca2a4
shalevc1098 <shalevc8901@gmail.com>
2026-01-02 17:45:39 +0200

    centered anydesk window

--- modules/anydesk/home.nix
+++ modules/anydesk/home.nix
@@ -11,6 +11,7 @@
     windowrule = [
       "match:class Anydesk, float on"
       "match:class Anydesk, size (monitor_w*0.6) (monitor_h*0.65)"
+      "match:class Anydesk, center on"
     ];
   };
 
