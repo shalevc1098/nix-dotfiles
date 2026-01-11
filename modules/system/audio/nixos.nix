a5895eb026f68f7d9aa00c54f1966b57174db2be
shalevc1098 <shalevc8901@gmail.com>
2026-01-11 21:19:57 +0200

    enabled pipewire jack and lowered audio quantum to 256

--- modules/system/audio/nixos.nix
+++ modules/system/audio/nixos.nix
@@ -4,13 +4,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     extraConfig.pipewire."99-rate" = {
       "context.properties" = {
         "default.clock.rate" = 48000;
         "default.clock.allowed-rates" = [ 48000 ];
-        "default.clock.quantum" = 1024;
-        "default.clock.min-quantum" = 1024;
-        "default.clock.max-quantum" = 1024;
+        "default.clock.quantum" = 256;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 256;
       };
     };
     extraConfig.pipewire."99-loopback" = {
