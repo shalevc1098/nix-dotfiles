3c37bbd618d6e28c2f4800f79017a5ce89561c10
shalevc1098 <shalevc8901@gmail.com>
2026-02-07 13:12:12 +0200

    added nix-ld libraries and LD_LIBRARY_PATH for CUDA

--- modules/system/nix-ld/nixos.nix
+++ modules/system/nix-ld/nixos.nix
@@ -1,3 +1,17 @@
+{ pkgs, ... }:
 {
   programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    curl
+    openssl
+  ];
+
+  environment.variables = {
+    LD_LIBRARY_PATH = [
+      "${pkgs.stdenv.cc.cc.lib}/lib"
+      "/run/opengl-driver/lib"
+    ];
+  };
 }
