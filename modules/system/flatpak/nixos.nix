c71e3e4cc97253f821d815b913dec0cdba66acd3
shalevc1098 <shalevc8901@gmail.com>
2026-02-13 17:00:53 +0200

    added flatpak nvidia GL runtime auto-install

--- modules/system/flatpak/nixos.nix
+++ modules/system/flatpak/nixos.nix
@@ -1,3 +1,13 @@
+{ config, pkgs, ... }:
+let
+  nvidiaVersion = builtins.replaceStrings [ "." ] [ "-" ]
+    config.hardware.nvidia.package.version;
+in
 {
   services.flatpak.enable = true;
+
+  # Auto-install matching Flatpak NVIDIA GL runtime on rebuild
+  system.activationScripts.flatpak-nvidia-gl.text = ''
+    ${pkgs.flatpak}/bin/flatpak install --noninteractive --system flathub org.freedesktop.Platform.GL.nvidia-${nvidiaVersion} 2>/dev/null || true
+  '';
 }
