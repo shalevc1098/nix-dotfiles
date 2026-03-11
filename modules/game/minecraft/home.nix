bc20f720255f381097cdc40f06d0eb9ef1a2afa6
shalevc1098 <shalevc8901@gmail.com>
2026-03-11 23:58:50 +0200

    added game module tweaks

--- modules/game/minecraft/home.nix
+++ modules/game/minecraft/home.nix
@@ -6,4 +6,17 @@
   home.packages = with pkgs; [
     prismlauncher
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    windowrule = [
+      "match:title ^Minecraft Console Edition$, float on"
+    ];
+  };
+
+  programs.niri.settings.window-rules = [
+    {
+      matches = [{ title = "^Minecraft Console Edition$"; }];
+      open-floating = true;
+    }
+  ];
 }
--- modules/game/steam/home.nix
+++ modules/game/steam/home.nix
@@ -1,4 +1,9 @@
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    protonup-qt
+  ];
+
   wayland.windowManager.hyprland.settings.windowrule = [
     "match:class steam title:^Steam.*, center on"
     "match:class steam_app_.*, center on"
