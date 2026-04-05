bd7494e1e1310ab077f60b0113611c650af29aab
shalevc1098 <shalevc8901@gmail.com>
2026-04-05 10:48:22 +0300

    added flatpak auto-update timer

--- modules/flatpak/nixos.nix
+++ modules/flatpak/nixos.nix
@@ -10,4 +10,22 @@ in
   system.activationScripts.flatpak-nvidia-gl.text = ''
     ${pkgs.flatpak}/bin/flatpak install --noninteractive --system flathub org.freedesktop.Platform.GL.nvidia-${nvidiaVersion} 2>/dev/null || true
   '';
+
+  # Auto-update Flatpak daily
+  systemd.services.flatpak-update = {
+    description = "Update Flatpak packages";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.flatpak}/bin/flatpak update -y --noninteractive";
+    };
+  };
+
+  systemd.timers.flatpak-update = {
+    description = "Auto-update Flatpak packages daily";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
 }
