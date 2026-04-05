0f5255c3d1f8c5f868a9e1c7612c7c7f4582b866
shalevc1098 <shalevc8901@gmail.com>
2026-04-05 10:48:10 +0300

    added sunshine module

---
+++ modules/sunshine/nixos.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  services.sunshine = {
+    enable = true;
+    openFirewall = true;
+    capSysAdmin = true;
+    package = pkgs.sunshine.override { cudaSupport = true; };
+  };
+}
