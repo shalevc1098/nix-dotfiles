da7c52adc18e5fdc44d1b4dfc5b71f8f2eb54b83
shalevc1098 <shalevc8901@gmail.com>
2026-04-05 10:48:44 +0300

    enabled nvidia open kernel module

--- modules/system/graphics/nixos.nix
+++ modules/system/graphics/nixos.nix
@@ -10,7 +10,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
   };
 }
