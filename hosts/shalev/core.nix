019001d5599dccc450956b916546f4ad5a3eaae3
shalevc1098 <shalevc8901@gmail.com>
2026-05-11 22:52:22 +0300

    added caches and disabled docs

--- hosts/shalev/core.nix
+++ hosts/shalev/core.nix
@@ -156,10 +156,12 @@ in
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
     download-buffer-size = 536870912;
     access-tokens = [
@@ -171,6 +173,10 @@ in
   nixpkgs.config = {
     allowUnfree = true;
   };
+
+  documentation.nixos.enable = false;
+  documentation.man.enable = false;
+  documentation.info.enable = false;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
