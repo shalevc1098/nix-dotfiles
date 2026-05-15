e4956ca7523e1a4523eb73819086c278e2ea3adb
shalevc1098 <shalevc8901@gmail.com>
2026-05-15 19:22:59 +0300

    declared firefox extensions and prefs

--- modules/firefox/home.nix
+++ modules/firefox/home.nix
@@ -16,33 +16,37 @@
       pkgs.firefoxpwa
       pkgs.pywalfox-native
     ];
-    policies = {
-      ExtensionSettings = {
-        "addon@darkreader.org" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-        };
-      };
-      Preferences = {
-        "gfx.webrender.all" = true;
-        "media.ffmpeg.vaapi.enabled" = true;
-        "media.hardware-video-decoding.force-enabled" = true;
-        "widget.dmabuf.force-enabled" = true;
-        "layers.acceleration.force-enabled" = true;
-      };
-    };
+
     package = pkgs.firefox.overrideAttrs (old: {
       postFixup = (old.postFixup or "") + ''
         wrapProgram $out/bin/firefox --set GTK_USE_PORTAL 1
       '';
     });
+
     profiles.default = {
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         bitwarden
         darkreader
+        export-cookies-txt
+        mal-sync
         pywalfox
+        tampermonkey
         ublock-origin
       ];
+
+      settings = {
+        "gfx.webrender.all" = true;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+        "widget.dmabuf.force-enabled" = true;
+        "layers.acceleration.force-enabled" = true;
+
+        "signon.rememberSignons" = false;
+        "browser.download.dir" = "${config.home.homeDirectory}/Downloads";
+        "browser.download.skipConfirmLaunchExecutable" = true;
+        "browser.translations.mostRecentTargetLanguages" = "en";
+        "browser.translations.neverTranslateLanguages" = "he";
+      };
     };
   };
 }
