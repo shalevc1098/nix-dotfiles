324ef6eb1a849fea13f1112e1f8e17fb2d04fa98
shalevc1098 <shalevc8901@gmail.com>
2026-06-19 00:19:20 +0300

    consolidated nixpkgs overlays into system module

--- modules/system/overlays/nixos.nix
+++ modules/system/overlays/nixos.nix
@@ -1,9 +1,27 @@
+{ inputs, ... }:
 {
   nixpkgs.overlays = [
     (final: prev: {
       openldap = prev.openldap.overrideAttrs {
         doCheck = !prev.stdenv.hostPlatform.isi686;
       };
+
+      matugen = inputs.matugen.packages.${prev.stdenv.hostPlatform.system}.default;
+    })
+
+    inputs.nur.overlays.default
+
+    (final: prev: {
+      zoom-us = prev.zoom-us.overrideAttrs (oldAttrs: {
+        dontPatchELF = true;
+        dontPreconcateArchives = true;
+        
+        fixupPhase = ''
+          runHook preFixup
+          echo "Skipping broken patchelf phase..."
+          runHook postFixup
+        '';
+      });
     })
   ];
 }
