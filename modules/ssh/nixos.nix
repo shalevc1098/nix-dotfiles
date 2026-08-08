43ac2e518e8b1a214970e9d5679c96bcdbad2df7
shalevc1098 <shalevc8901@gmail.com>
2026-08-08 09:49:35 +0300

    declared truenas ssh host key

--- modules/ssh/nixos.nix
+++ modules/ssh/nixos.nix
@@ -7,5 +7,15 @@
   programs.ssh = {
     startAgent = false;
     askPassword = lib.mkForce "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
+
+    # restic runs as root and would otherwise stop on the host key prompt
+    knownHosts.truenas = {
+      hostNames = [
+        "truenas"
+        "100.108.96.85"
+        "192.168.1.192"
+      ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA7+5bUGuiyJ81XuSVa3wcEyXcHNWeVckOhDF8eVT0KX";
+    };
   };
 }
